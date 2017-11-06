18f3a101fcb7f4cc0a9de7a14202ca4633246091
Orlando Rodrigues <djcoper.github.io@gmail.com>
2017-11-06 13:31:47 +0000

    linear-gradient

---
+++ linear-gradient/linear-gradient.js
@@ -0,0 +1,53 @@
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	colorMode(HSB); strokeWeight(5);
+}
+let dir = 1;
+let range = 500;
+let count = 0;
+function draw() {
+	if (frameCount % 1 === 0 ) {
+		count += dir;
+		morph();
+	}
+	if (count > range*2 || count < -range/2) {
+		dir *=-1;
+	}
+}
+function morph() {
+	translate(width/2, height/2);
+	let hue = map(mouseY, 0,height, -30,310);
+	let rotation = 0;
+	rotation += map(count, 0,width, -0.1,0.1);
+	//rotation += map(mouseY, -height,height, -0.2,0.2);
+	background(255);
+
+	stroke(50);
+	for (let x = -width; x < width; x+= 100) {
+		for (let y = -height; y < height/2; y+=100) {
+			line(-width,y,width/2,y);
+			line(x,-height,x,height/2);
+		}
+	}
+
+	for (let y = -height/2; y < 10; y+= 2.5) {
+		hue += 0.5; rotate(rotation);
+		stroke(hue,100,100);
+		line (-width/2,y, width/2,y);
+		push()
+		strokeWeight(10);
+		point(-width/2,y, 10);
+		point(width/2,y, 10);
+		//stroke(hue,100,100);
+		fill(hue,100,100)
+		point(0,25);
+		pop();
+	}
+	text("Rotation : "+rotation, 0,10);
+}
+function windowResized () {
+	resizeCanvas(
+		windowWidth,
+		windowHeight
+	);
+}
--- staffgame/game_code.js
+++ staffgame/game_code.js
@@ -1,100 +1,100 @@
 //////////////////////////////////
-varstaff_game_setup = false;//
-varsetup = display;//
+var staff_game_setup = false;//
+var setup = display;//
 //////////////////////////////////
 
-varmode = 1;
+var mode = 1;
 
-varmouseWasClicked = false;
-varbackground_color = [255];
-vardistance;varx;vary;varspeed = 4;
-varXspeed = speed;varYspeed = speed-1;
-vargame_score = 0;vartime = 1;
-varGameOver = true;vargo = 0;
-varsaid_gameOver = false;varo = false;
-varpick_image;
-//varbg_speed = 1;
-//varbg = 255;
-//varprfix = "game_assets/";
+var mouseWasClicked = false;
+var background_color = [255];
+var distance;var x;var y;var speed = 4;
+var Xspeed = speed;var Yspeed = speed-1;
+var game_score = 0;var time = 1;
+var GameOver = true;var go = 0;
+var said_gameOver = false;var o = false;
+var pick_image;
+//var bg_speed = 1;
+//var bg = 255;
+//var prfix = "game_assets/";
 
-vardraw = staff_game;
+var draw = staff_game;
 
-varimg = [
+var img = [
 "game_assets/",
 "milon","kamal","leo",
 "mike","panos","radek",
 "ray","wazir",
 ];
-varsfx = [];
-functionpreload() {
+var sfx = [];
+function preload() {
 	sfx[0] = loadSound(img[0]+"click.mp3")
 	sfx[9] = loadSound(img[0]+"new_game.mp3")
 	sfx[10] = loadSound(img[0]+"start_game.mp3")
 
-	for(leti = 1;i<img.length;i++) {
+	for(var i = 1;i<img.length;i++) {
 		sfx[i] = loadSound(img[0]+img[i]+".mp3");
 		img[i] = loadImage(img[0]+img[i]+".png");
 	}
 }
-functiondisplay() {
+function display() {
 	createCanvas(window.innerWidth,window.innerHeight);
 	sfx[10].play();
 }
-functionstaff_game() {
-if (!staff_game_setup) {
-	x = width/2;
-	y = height/2
-	imageMode(CENTER);rectMode(CENTER);noStroke();
-	pick_image = Math.floor(random(1,img.length));
-	//mode_slider = createSlider(1,3,2);
-	//mode_slider.position(width/2-67,height-150);
+function staff_game() {
+	if (!staff_game_setup) {
+		x = width/2;
+		y = height/2
+		imageMode(CENTER);rectMode(CENTER);noStroke();
+		pick_image = Math.floor(random(1,img.length));
+		//mode_slider = createSlider(1,3,2);
+		//mode_slider.position(width/2-67,height-150);
 ////////////////////////////////
-	staff_game_setup = true;//
+		staff_game_setup = true;//
 ////////////////////////////////
 //console.log(staff_game_setup);
-}
-background(background_color);
-//ellipse(width/2,height-135,150,150);
-//bg- = bg_speed
-//console.log(bg);
-if (GameOver&&time>0) {
+	}
+	background(background_color);
+	//ellipse(width/2,height-135,150,150);
+	//bg- = bg_speed
+	//console.log(bg);
+	if (GameOver&&time>0) {
 
 /////////////GAMEMODE//////////
 	ellipse(width/2-200,height-100,50,50);
 	ellipse(width/2,height-100,50,50);
 	ellipse(width/2+200,height-100,50,50);
 
-	if (mode =  =  = 1) {
+	if (mode === 1) {
 		speed = 1;
-}elseif (mode =  =  = 2) {
-speed = 4;
-}elseif (mode =  =  = 3) {
-speed = 6;
-}
+	}else if (mode === 2) {
+		speed = 4;
+	}else if (mode === 3)
+	speed = 6;
+	}
 
 /////////////GAMEMODE//////////
 
-if (frameCount%40 =  =  = 0) {fill(random(200),random(200),random(200));}
-textSize(80);textStyle(BOLD);
-text("Orlando'sStaffGame!",width/2-430,height/2-50);
-textSize(23);push();
-fill(0);
-text("PUSHSPACEBARTOSTARTNEWGAME",width/2-230,height/2+50);
-pop();}
-if (!GameOver) {
-//console.log(speed);
-//mode_slider.position(10,-100);
-//rect(x,y,130,190)
-if (!GameOver) {
-//ellipse(x,y,190,190);
-image(img[pick_image],x,y,130,190);
-}
-if (mouseX>0&&mouseY>0&&dist(width/2,height-135,mouseX,mouseY)>75)
-{fill(255,0,0);ellipse(mouseX+3,mouseY+5,30,30);}
+	if (frameCount%40 === 0) {fill(random(200),random(200),random(200));}
+	textSize(80);textStyle(BOLD);
+	text("Orlando'sStaffGame!",width/2-430,height/2-50);
+	textSize(23);push();
+	fill(0);
+	text("PUSHSPACEBARTOSTARTNEWGAME",width/2-230,height/2+50);
+	pop();}
+//	if (!GameOver) {
+		//console.log(speed);
+		//mode_slider.position(10,-100);
+		//rect(x,y,130,190)
+		if (!GameOver) {
+			//ellipse(x,y,190,190);
+			image(img[pick_image],x,y,130,190);
+		}
+// if (mouseX>0&&mouseY>0&&dist(width/2,height-135,mouseX,mouseY)>75)
+// {fill(255,0,0);ellipse(mouseX+3,mouseY+5,30,30);}
 x += Xspeed;
 y += Yspeed;
 if (x>width-50||x<50) {
-Xspeed* = -1;newStaff();
+Xspeed*= -1;newStaff();
 //pick_image = Math.floor(random(1,img.length));
 //speed = Math.floor(random(20,60));
 //console.log(speed);
@@ -102,7 +102,7 @@ Xspeed* = -1;newStaff();
 //sfx[pick_image].play();
 }
 if (y>height-90||y<90) {
-Yspeed* = -1;newStaff();
+Yspeed*= -1;newStaff();
 //pick_image = Math.floor(random(1,img.length));
 //console.log(pick_image);
 //sfx[pick_image].play();
@@ -117,8 +117,8 @@ fill(0);
 textSize(30);
 text("Score:"+game_score,10,30);
 text("Time:"+time,width-130,30);pop();
-if (!GameOver&&frameCount%60 =  = 0) {time--}
-}
+if (!GameOver&&frameCount%60 == 0) {time--}
+//}
 if (time<1) {GameOver = true;
 background_color = [255]
 fill(0);
@@ -135,21 +135,21 @@ said_gameOver = true;
 //console.log(said_gameOver);
 }
 }
-}
+//}
 
 //sfx[9].play();
 
-functionnewStaff() {
+function newStaff() {
 	pick_image = Math.floor(random(1,img.length));
-	if (pick_image =  = 1) {
+	if (pick_image == 1) {
 		sfx[pick_image].setVolume(2.0);
 	}else{
 		sfx[pick_image].setVolume(3.0);
 	}
-	if (pick_image =  = 5) {
+	if (pick_image == 5) {
 		sfx[pick_image].setVolume(9.0);
 	}
-	if (pick_image =  = 2) {
+	if (pick_image == 2) {
 		sfx[pick_image].setVolume(0.0);
 	}
 	//else{sfx[pick_image].setVolume(5.0);}
@@ -158,9 +158,9 @@ functionnewStaff() {
 
 
 
-functionkeyPressed() {
+function keyPressed() {
 	//if (key = null;) {;game_score = 0;GameOver = false;time = 20;}
-	if (key =  = ""&&GameOver) {
+	if (key == ""&&GameOver) {
 		game_score = 0;GameOver = false;time = 20;
 		x = width/2;
 		y = height/2
@@ -169,11 +169,11 @@ functionkeyPressed() {
 	}
 }
 
-functionmousePressed() {
+function mousePressed() {
 
-	var ellipse1 = dist(width/2-200,height-100,mouseX,mouseY);
-	var ellipse2 = dist(width/2,height-100,mouseX,mouseY);
-	var ellipse3 = dist(width/2+200,height-100,mouseX,mouseY);
+	var  ellipse1 = dist(width/2-200,height-100,mouseX,mouseY);
+	var  ellipse2 = dist(width/2,height-100,mouseX,mouseY);
+	var  ellipse3 = dist(width/2+200,height-100,mouseX,mouseY);
 
 	if (ellipse1<25) {
 		console.log("Button1waspressed");
@@ -183,8 +183,8 @@ functionmousePressed() {
 		console.log("Button3waspressed");
 	}
 
-	//varmode = mode_slider.value()
-	//if (mode =  = 3) {speed = 50;}
+	//var mode = mode_slider.value()
+	//if (mode == 3) {speed = 50;}
 
 	//console.log(width/2);
 	/**if (x>width/2+1||x<width/2-1&&said_gameOver<2) {sfx[9].setVolume(0.5);
@@ -213,7 +213,7 @@ functionmousePressed() {
 	}
 }
 
-functionwindowResized() {
+function windowResized() {
 	resizeCanvas(
 		window.innerWidth,
 		window.innerHeight
