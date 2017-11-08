b7b977c21b34a9891e58a3cc2b32cd7d9c79dc93
Orlando Rodrigues <djcoper.github.io@gmail.com>
2017-11-08 23:04:18 +0000

    Added Buzzkill game

---
+++ Buzzkill/buzzkill.js
@@ -0,0 +1,105 @@
+let bees = [];
+let score = 0;
+let time = 20;
+let gameover = false;
+function preload () {
+	beeImg = loadImage("assets/Queen_Bee.png");
+	bg = loadImage("assets/bg.jpg");
+	sq = loadImage("assets/sq.png");
+	hand = loadImage("assets/hand.png");
+}
+function setup () {
+	createCanvas(window.innerWidth, window.innerHeight);
+	textSize(23);
+}
+function mousePressed () {
+	if(gameover){
+		score = 0; time = 20;
+		textSize(23);
+		gameover = false;
+	}
+}
+function draw () {
+	image(bg, 0,0 ,width,height);
+	if(!gameover){
+		for(let i = 0 ; i < bees.length; i ++){
+			bees[i].Render();
+			if( bees[i].x < -100 || bees[i] > height){
+				bees . splice ( i , 1 );
+			}
+			if(dist(bees[i].x,bees[i].y,mouseX,mouseY) < 35 ) {
+				bees[i].speed = 0;
+				bees[i].fall = random(8,15);
+				if(!bees[i].hit){
+					score ++;
+				}
+				bees[i].hit = true;
+			}
+		}
+		if (frameCount % 10 === 0 ) {
+			bees.push (
+				new Bee (
+					random(width, width*2),
+					random(height)
+				)
+			);
+		}
+		fill(255);
+		text( "SCORE : " + score, 10,30);
+		text("TIME : " +time, width-100,30);
+		if(frameCount%60===0){
+			time--;
+		}
+		image(hand,mouseX-20,mouseY-30,60,60);
+	}
+	if(time < 1){
+		gameover=true;
+	}
+	if(gameover){
+		textSize(50);
+		text("GAME OVER", width/2 - 150,height/2);
+		text("SCORE : " + score, width/2 -134,height/2+50);
+	}
+}
+function Bee (x, y) {
+	this.hit = false;
+	this.x = x;
+	this.y = y;
+	this.size = 50;
+	this.speed = random(7,16);
+	this.fall = 0;
+	this.up = random(1,2);
+	this.Render = function() {
+		//ellipse(this.x, this.y, 10);
+		this.Update();
+		if(this.hit) {
+			image(
+				sq,
+				this.x,
+				this.y += random(-2,2),
+				this.size,this.size+=0.5
+			);
+		} else {
+			image(
+				beeImg,
+				this.x - beeImg.width/2/2,
+				this.y -= random(-2,2),
+				this.size,this.size
+			);
+		}
+	}
+	this.Update = function () {
+		this.x -= this.speed;
+		this.y += this.fall;
+		this.y += this.up;
+		if(frameCount%60===0){
+			this.up*=-1;
+		}
+	}
+}
+function windowResized () {
+	resizeCanvas(
+		window.innerWidth,
+		window.innerHeight
+	);
+}
