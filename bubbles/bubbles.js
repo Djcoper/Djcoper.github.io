07c50a373a78f97ae5d44e3bb20742f5b11c52a9
Orlando Rodrigues <djcoper.github.io@gmail.com>
2017-11-24 11:14:23 +0000

    clear bubbles in bubbles js file

--- bubbles/bubbles.js
+++ bubbles/bubbles.js
@@ -1,29 +1,24 @@
 const
 	bubbles = [];
-	//col = [0,100,100];
-let
-	vol;
+//col = [0,100,100];
+// let
+//vol;
 function setup () {
 	createCanvas(
 		window.innerWidth,
 		window.innerHeight
 	);
-
 	imageMode(CENTER);
 	bubble = loadImage("bubble.png");
-
 	//colorMode(HSB);
-
 	//fill(255); //noStroke();
-
 	//mic = new p5.AudioIn();
 	//mic.start();
 }
-
 function draw () {
 	background(38, 50, 56);
 	//hsl background(220,19,18);
-	for (var i = bubbles.length-1; i >= 0; i--) {
+	for (let i = bubbles.length-1; i >= 0; i--) {
 		bubbles[i].Render();
 		if (
 			(bubbles[i].y < -100)
@@ -40,31 +35,29 @@ function draw () {
 			bubbles.splice(i,1);
 		}
 	}
-
 	if (mouseIsPressed) {
 		//col[0] ++;
-		for (var i = 0; i < 2; i++) {
+		for (let i = 0; i < 2; i++) {
 			bubbles.push ( new Bubble (mouseX, mouseY) );
 		}
 	}
-
-	for (var i = 0; i < 6; i++) {
-		bubbles.push ( new Bubble (width/2, -50, 1));
+	if (!mouseIsPressed) {
+		for (let i = 0; i < 5; i++) {
+			bubbles.push ( new Bubble (width/2, -50, 1));
+		}
+		for (let i = 0; i < 10; i++) {
+			//bubbles.push ( new Bubble (width/2, height/2));
+			bubbles.push ( new Bubble (random(100,width-100), height +50));
+		}
 	}
 	// vol = mic.getLevel();
 	// if (vol > 0.001) {
 	// 	//col[0] ++;
-	// 	for (var i = 0; i < 13; i++) {
+	// 	for (let i = 0; i < 13; i++) {
 	// 		//bubbles.push ( new Bubble (width/2, height/2));
 	// 		bubbles.push ( new Bubble (random(100,width-100), height +300));
 	// 	}
 	// }
-
-		for (var i = 0; i < 13; i++) {
-			//bubbles.push ( new Bubble (width/2, height/2));
-			bubbles.push ( new Bubble (random(100,width-100), height +50));
-		}
-
 	// if (col[0] > 300) {
 	// 	col[0] = 0;
 	// }
@@ -96,33 +89,29 @@ function Bubble (x,y,d) {
 		// 	this.y += this.yDir - 4,
 		// 	this.size
 		// )
-
 		image(
 			bubble,
 			this.x += this.xDir -= this.dir,
 			this.y += this.yDir -= 0.1,
 			this.size,this.size
 		)
-
 		if (d > 0) {
 			this.y += this.yDir += 10;
 		}
-
 		if (!mouseIsPressed&&dist(this.x, this.y, xs,ys) < width*1.1) {
 			this.yDir = 1;
-
 			ys -= 0.002;
 		}
-
-		if (ys < -1285) {
+		if (ys < -width*1.1) {
 			ys = 173;
 		}
+		//ellipse(xs,ys,10)
 	}
 }
-
 function windowResized() {
 	resizeCanvas(
 		window.innerWidth,
 		window.innerHeight
 	);
+	xs = window.innerWidth/2
 }
