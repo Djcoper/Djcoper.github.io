0e359a3f51c87cf23a8494349d644cd1ae59a4f3
Orlando P Rodrigues <djcoper.github.io@gmail.com>
2019-06-02 18:47:14 +0100

    add colR to tenprint

--- tenprint/tenprint.js
+++ tenprint/tenprint.js
@@ -1,25 +1,22 @@
+const area = 30; colR = [74, 154, 239];
 function setup () {
-	createCanvas(window.innerWidth,window.innerHeight);
-	stroke(255); strokeWeight(10);
-	background(74, 154, 239);
+	createCanvas (window.innerWidth, window.innerHeight);
+	stroke (255); strokeWeight (10); background (colR);
 }
-const area = 30;
-let x = 0;
-let y = 0;
-const time = 60;
+let x = 0; y = 0;
 function draw () {
-	if (frameCount % time === 0) {
-		background(74, 154, 239);
-		y = 0;
+	if (frameCount % 60 === 0) {
+		colR = [random(255),random(255),random(255)];
+		background(colR); y = 0;
 	}
 	while (y < height) {
-		if (random (1) < 0.5 ) {
-			line(x,y, x+area,y+area);
+		if (random (1) < .5 ) {
+			line (x, y, x + area, y + area);
 		} else {
-			line(x+area,y, x,y+area);
+			line (x + area, y, x, y+area);
 		}
 		if (x > width) {
-			x = -area; y += area;
+			x =- area; y += area;
 		}
 		x += area;
 	}
